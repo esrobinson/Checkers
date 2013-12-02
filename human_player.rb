0824a88355fe7cb8c745acccdc1c35eefdeb1283
App Academy Student <student@appacademy.io>
2013-12-02 14:03:56 -0800

    Adds HumanPlayer

---
+++ human_player.rb
@@ -0,0 +1,23 @@
+class HumanPlayer
+  attr_reader :name, :color
+
+  def initialize(name)
+    @name = name
+  end
+
+  def playing_as(color)
+    @color = color
+  end
+
+  def move
+    puts "Make a move (FORMAT: 0, 0; 1, 1; 0, 2;)"
+    move = gets.chomp
+    parse_move(move)
+  end
+
+  def parse_move(move)
+    positions = move.split(';')
+    positions.map{ |pos| pos.split(',').map(&:to_i)}
+  end
+
+end
